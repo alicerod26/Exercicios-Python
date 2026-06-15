8cb000a9c4735f2fa88125313b9205285695f440
Alicerod26 <alicerod842@gmail.com>
2026-06-15 11:48:10 -0300

    add: Trabalhando com janelas

---
+++ .venv/Python/janela2.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+import sys
+
+class Janela2(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Minha Janela")
+        #Largura, altura e posição da janela
+        self.setGeometry(50,80,800,400)
+
+app = QApplication(sys.argv)
+tela = Janela2()
+tela.show()
+app.exec()
+
